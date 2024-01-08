b51dc08cb38491e5688f45ef9bb8876f789e3dae
Ali Chraghi <alichraghi@proton.me>
2024-01-08 19:36:49 +0330

    update to latest build API

--- build.zig
+++ build.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("vulkan-zig-generated", .{ .source_file = .{ .path = "vk.zig" } });
+    _ = b.addModule("vulkan-zig-generated", .{ .root_source_file = .{ .path = "vk.zig" } });
 }
