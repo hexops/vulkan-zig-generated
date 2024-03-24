bfbfa2f978936c6495f72a4baa554d54b6566550
Stephen Gutekanst <stephen@hexops.com>
2024-03-24 12:30:59 -0700

    update to latest
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- update.sh
+++ update.sh
@@ -8,9 +8,19 @@ mkdir vulkan-zig/
 pushd vulkan-zig
 git init
 git remote add origin https://github.com/Snektron/vulkan-zig
-git fetch --depth 1 origin 4f15927ba2e368b26c61c14deab9938bf5b19b8d
+git fetch --depth 1 origin ac4103a733c479b599aae8d42c08cabd7d5cf48a
 git checkout FETCH_HEAD
 popd
 
 # Generate vulkan bindings
 zig run vulkan-zig/generator/main.zig -- vk.xml vk.zig
+
+echo "
+// -------------------------------------------
+
+pub const ANativeWindow = opaque{};
+pub const CAMetalLayer = opaque{};
+pub const AHardwareBuffer = opaque{};
+" >> vk.zig
+
+zig fmt vk.zig
